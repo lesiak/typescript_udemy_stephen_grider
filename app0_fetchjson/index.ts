83cae4b5cbfaa3f87ee5fe1dc507b3ca325bb6fe
lesiak <lesiak@outlook.com>
2019-09-24 07:48:14 +0200

    [APP0] Interface

--- app0_fetchjson/index.ts
+++ app0_fetchjson/index.ts
@@ -2,6 +2,20 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  console.log(response.data);
+  const todo: Todo = response.data;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+  console.log(`
+    The Todo with id: ${id}
+    has a title of: ${title}
+    is completed?: ${completed}
+  `);
 });
