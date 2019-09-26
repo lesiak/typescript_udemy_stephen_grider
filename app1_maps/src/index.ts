01e8047a2da07b0e554f7f38c214798e8494c6f1
lesiak <lesiak@outlook.com>
2019-09-26 12:25:53 +0200

    [APP1-MAPS] Added Company class

--- app1_maps/src/index.ts
+++ app1_maps/src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './user';
+import { Company } from './company';
 
 console.log('hi there');
 const user = new User();
+const company = new Company();
 console.log(user);
+console.log(company);
