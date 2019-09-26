62aef502ba60a4b5d8d3d652bcae18d9997b1e09
lesiak <lesiak@outlook.com>
2019-09-26 18:09:05 +0200

    [APP1-MAPS] Displayed map on screen

--- app1_maps/src/index.ts
+++ app1_maps/src/index.ts
@@ -4,5 +4,11 @@ import { Company } from './company';
 console.log('hi there');
 const user = new User();
 const company = new Company();
-console.log(user);
-console.log(company);
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0
+  }
+});
