1486c5c75aa3902287c85a9bc1c059f9c6a79597
lesiak <lesiak@outlook.com>
2019-10-01 08:53:28 +0200

    [APP2-SORT] LinkedList implementation

--- app2_sort/src/index.ts
+++ app2_sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './numbersCollection';
 import { CharactersCollection } from './charactersCollection';
+import { LinkedList } from './linkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const numbersSorter = new Sorter(numbersCollection);
@@ -11,3 +12,12 @@ const charactersCollection = new CharactersCollection('XaayBb');
 const stringSorter = new Sorter(charactersCollection);
 stringSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
