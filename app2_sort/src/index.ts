90394a8f1858a77f299e807508a9ca7a104949be
lesiak <lesiak@outlook.com>
2019-10-01 07:55:05 +0200

    [APP2-SORT] Abstracted compare and swap

--- app2_sort/src/index.ts
+++ app2_sort/src/index.ts
@@ -1,28 +1,7 @@
-class Sorter {
-  constructor(public collection: number[] | string) {}
+import { Sorter } from './sorter';
+import { NumbersCollection } from './numbersCollection';
 
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // number[]
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        // string
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, 3, -5, 0]);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
