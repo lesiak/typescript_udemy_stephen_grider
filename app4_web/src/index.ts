4531fdabd38915cb772ddfc007b5db47607e499a
lesiak <lesiak@outlook.com>
2019-10-07 18:56:19 +0200

    [APP4-WEB] Basic model class
    - only one model class
    - property getter
    - property setter

--- app4_web/src/index.ts
+++ app4_web/src/index.ts
@@ -1,1 +1,6 @@
-console.log('Hello world');
+import { User } from './models/user';
+
+const user = new User({ name: 'John', age: 20 });
+user.set({ name: 'Jack' });
+console.log(user.get('age'));
+console.log(user.get('name'));
