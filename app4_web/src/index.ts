7350e2e434128649e0a174f42616e90c09eb941b
lesiak <lesiak@outlook.com>
2019-10-08 21:00:44 +0200

    [APP4-WEB] Get user data

--- app4_web/src/index.ts
+++ app4_web/src/index.ts
@@ -1,20 +1,4 @@
 import { User } from './models/user';
 
-const user = new User({ name: 'John', age: 20 });
-user.set({ name: 'Jack' });
-console.log(user.get('age'));
-console.log(user.get('name'));
-
-user.on('change', () => {
-  console.log('Change #1');
-});
-
-user.on('change', () => {
-  console.log('Change #2');
-});
-
-user.on('save', () => {
-  console.log('Save');
-});
-
-user.trigger('change');
+const user = new User({ id: 1 });
+user.fetch();
