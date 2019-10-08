386e203353695e76692c36062e264bee61d3fe21
lesiak <lesiak@outlook.com>
2019-10-08 20:38:25 +0200

    [APP4-WEB] Event listeners for model

--- app4_web/src/index.ts
+++ app4_web/src/index.ts
@@ -4,3 +4,17 @@ const user = new User({ name: 'John', age: 20 });
 user.set({ name: 'Jack' });
 console.log(user.get('age'));
 console.log(user.get('name'));
+
+user.on('change', () => {
+  console.log('Change #1');
+});
+
+user.on('change', () => {
+  console.log('Change #2');
+});
+
+user.on('save', () => {
+  console.log('Save');
+});
+
+user.trigger('change');
