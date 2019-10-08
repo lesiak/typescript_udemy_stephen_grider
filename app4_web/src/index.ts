aefb17e92f640b76496c8b8a704c634dec433a7b
lesiak <lesiak@outlook.com>
2019-10-08 21:11:25 +0200

    [APP4-WEB] Save user data

--- app4_web/src/index.ts
+++ app4_web/src/index.ts
@@ -1,4 +1,7 @@
 import { User } from './models/user';
 
-const user = new User({ id: 1 });
-user.fetch();
+const user = new User({ id: 1, name: 'John', age: 50 });
+user.save();
+
+const userNew = new User({ name: 'Jim', age: 55 });
+userNew.save();
