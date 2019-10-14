8d4c52968ff48628409999460b86f5e5ef84f98e
lesiak <lesiak@outlook.com>
2019-10-14 11:00:29 +0200

    [APP4-WEB] Added UserForm

--- app4_web/src/index.ts
+++ app4_web/src/index.ts
@@ -1,20 +1,24 @@
 import { User } from './models/user';
+import { UserForm } from './views/userForm';
 
-const user = User.buildUser({ id: 1, name: 'John', age: 50 });
-//user.save();
-console.log(user.get('name'));
-user.on('change', () => {
-  console.log(user);
-});
-user.set({ name: 'Jim' });
-const user2 = User.buildUser({ id: 2 });
-user2.on('change', () => {
-  console.log(user2);
-});
-user2.fetch();
+// const user = User.buildUser({ id: 1, name: 'John', age: 50 });
 
-const collection = User.buildUserCollection();
-collection.on('change', () => {
-  console.log(collection);
-});
-collection.fetch();
+// console.log(user.get('name'));
+// user.on('change', () => {
+//   console.log(user);
+// });
+// user.set({ name: 'Jim' });
+// const user2 = User.buildUser({ id: 2 });
+// user2.on('change', () => {
+//   console.log(user2);
+// });
+// user2.fetch();
+
+// const collection = User.buildUserCollection();
+// collection.on('change', () => {
+//   console.log(collection);
+// });
+// collection.fetch();
+
+const userForm = new UserForm(document.getElementById('root'));
+userForm.render();
