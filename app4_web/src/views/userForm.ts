81edbf0c4db678c8d98a1c61b34b0ea1ed69fd66
lesiak <lesiak@outlook.com>
2019-10-14 14:37:51 +0200

    [APP4-WEB] Binding event handlers

--- app4_web/src/views/userForm.ts
+++ app4_web/src/views/userForm.ts
@@ -1,11 +1,22 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  private eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick
+    };
+  }
+
+  onButtonClick(): void {
+    console.log('Hi there');
+  }
+
   template(): string {
     return `
     <div>
       <h1>User Form</h1>
       <input />
+      <button>Click Me</button>
     </div>
    `;
   }
@@ -13,6 +24,17 @@ export class UserForm {
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
     this.parent.append(templateElement.content);
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 }
