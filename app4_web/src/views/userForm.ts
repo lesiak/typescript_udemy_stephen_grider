e6a60429ab0e95f6c33fc633cd2f8257a0f90d4c
lesiak <lesiak@outlook.com>
2019-10-15 07:34:34 +0200

    [APP4-WEB] Extracted View class

--- app4_web/src/views/userForm.ts
+++ app4_web/src/views/userForm.ts
@@ -1,13 +1,21 @@
 import { User } from '../models/user';
+import { View } from './view';
 
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.model.on('change', () => {
-      this.render();
-    });
+export class UserForm extends View {
+  template(): string {
+    return `
+    <div>
+      <h1>User Form</h1>
+      <div>User name: ${this.model.get('name')}</div>
+      <div>User age: ${this.model.get('age')}</div>
+      <input />
+      <button id='set-name'>Change Name</button>
+      <button id='set-age'>Set Random Age</button>
+    </div>
+   `;
   }
 
-  private eventsMap(): { [key: string]: () => void } {
+  eventsMap(): { [key: string]: () => void } {
     return {
       'click:button#set-age': this.onSetAgeClick,
       'click:button#set-name': this.onSetNameClick
@@ -25,35 +33,4 @@ export class UserForm {
       this.model.set({ name });
     }
   };
-
-  template(): string {
-    return `
-    <div>
-      <h1>User Form</h1>
-      <div>User name: ${this.model.get('name')}</div>
-      <div>User age: ${this.model.get('age')}</div>
-      <input />
-      <button id='set-name'>Change Name</button>
-      <button id='set-age'>Set Random Age</button>
-    </div>
-   `;
-  }
-
-  render(): void {
-    this.parent.innerHTML = '';
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-    this.bindEvents(templateElement.content);
-    this.parent.append(templateElement.content);
-  }
-
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-    for (const eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
-      fragment.querySelectorAll(selector).forEach(element => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
 }
---
+++ app4_web/src/views/view.ts
@@ -0,0 +1,30 @@
+import { User } from '../models/user';
+
+export abstract class View {
+  abstract template(): string;
+  abstract eventsMap(): { [key: string]: () => void };
+
+  constructor(public parent: Element, public model: User) {
+    this.model.on('change', () => {
+      this.render();
+    });
+  }
+
+  render(): void {
+    this.parent.innerHTML = '';
+    const templateElement = document.createElement('template');
+    templateElement.innerHTML = this.template();
+    this.bindEvents(templateElement.content);
+    this.parent.append(templateElement.content);
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+    for (const eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+      fragment.querySelectorAll(selector).forEach(element => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
+  }
+}
