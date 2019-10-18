dbe9d13da94229c5147459278d0fdf913accfcb1
lesiak <lesiak@outlook.com>
2019-10-18 08:56:44 +0200

    [APP5-EXPRESS] cookieSession

--- app5_express/src/index.ts
+++ app5_express/src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['fdggdhwhsw'] }));
 app.use(router);
 app.listen(3000, () => {
   console.log('Listening on port 3000');
--- app5_express/src/routes/loginRoutes.ts
+++ app5_express/src/routes/loginRoutes.ts
@@ -24,10 +24,11 @@ router.get('/login', (req: Request, res: Response) => {
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
-  if (email) {
-    res.send(email.toUpperCase());
+  if (email === 'hi@hi.com' && password === 'password') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
   } else {
-    res.send('You must provide email');
+    res.send('Invalid email or password');
   }
 });
 
