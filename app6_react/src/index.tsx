9df79e38de60f75963ccd76d902ebf2637a9200b
lesiak <lesiak@outlook.com>
2019-11-04 14:30:08 +0100

    [APP6-REACT] Component state

--- app6_react/src/index.tsx
+++ app6_react/src/index.tsx
@@ -6,8 +6,26 @@ interface AppProps {
 }
 
 class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi there, {this.props.color}</div>;
+    return (
+      <div>
+        Hi there, {this.props.color}
+        <br />
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
