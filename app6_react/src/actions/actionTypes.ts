9ef18aeef8d373b654c4dfa19c39a2e76d9126da
lesiak <lesiak@outlook.com>
2019-11-05 09:26:51 +0100

    [APP6-REACT] Deleting a Todo

--- app6_react/src/actions/actionTypes.ts
+++ app6_react/src/actions/actionTypes.ts
@@ -1,3 +1,22 @@
 export enum ActionTypes {
-  fetchTodos
+  fetchTodos,
+  deleteTodo
 }
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
+export type Action = FetchTodosAction | DeleteTodoAction;
